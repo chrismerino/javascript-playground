45a7cec2ca1149380b5161d0f84cd4f015eefc3e
Chris Merino <cris.merino@live.com>
2020-07-23 17:33:27 -0600

    functions topic almost completed! yay!

--- custom-functions/customfunction.js
+++ custom-functions/customfunction.js
@@ -1,12 +1,4 @@
 // Function definition
-function calculateBill(billAmmount, taxRate) {
-  // this is a function body
-  console.log('Running Calculate Bill!!');
-  const total = billAmmount * (1 + taxRate);
-  console.log(total);
-  return total;
+function calculateBill(billAmmount, taxRate = 0.13, tipRate = 0.15) {
+  return billAmmount + billAmmount * taxRate + billAmmount * tipRate;
 }
-
-const totalReturned = calculateBill(100, 0.13);
-const totalReturned2 = calculateBill(200, 0.13);
-console.log(`Your total is $${totalReturned}, and total2 is ${totalReturned2}`);
