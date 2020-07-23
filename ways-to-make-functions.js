7ba6e87de27c8ece3d5c974ec8c078e51c5a2fa6
Chris Merino <cris.merino@live.com>
2020-07-23 17:50:28 -0600

    learning how to implement functions

---
+++ ways-to-make-functions.js
@@ -0,0 +1,9 @@
+// Anonymous Function
+// function (firstName) {
+//   return `Dr. ${firstName}`;
+// }
+
+// Function Expression
+const doctorize = function(firstName) {
+  return `Dr. ${firstName}`;
+};
