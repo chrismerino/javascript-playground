0b60ea77273e3bf10774e323084ac38bc661e991
Chris Merino <cris.merino@live.com>
2020-07-24 09:45:14 -0600

    going through ways to make functions!

--- ways-to-make-functions.js
+++ ways-to-make-functions.js
@@ -3,7 +3,15 @@
 //   return `Dr. ${firstName}`;
 // }
 
-// Function Expression
-const doctorize = function(firstName) {
-  return `Dr. ${firstName}`;
-};
+/* // Function Expression
+function inchToCM(inches) {
+  return inches * 2.54;
+} */
+
+/* // Anonymous Function
+const inchToCM = function(inches) {
+  return inches * 2.54;
+}; */
+
+// Arrow Function
+const inchToCM = inches => inches * 2.54;
