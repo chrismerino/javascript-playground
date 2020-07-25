24e79600a44186bb77b2329c6702e797a09e5289
Chris Merino <cris.merino@live.com>
2020-07-25 10:37:55 -0600

    testing scope

--- scope.js
+++ scope.js
@@ -1,60 +1,13 @@
-const people = [
-  { name: 'Wes', cool: true, country: 'Canada' },
-  { name: 'Scott', cool: true, country: 'Merica' },
-  { name: 'Snickers', cool: false, country: 'Dog Country' },
-];
+// const age = 100;
 
-people.forEach((person, index) => {
-  console.log(person.name);
-});
+// function go() {
+//   const hair = 'blonde';
+// }
 
-console.table(people);
+// console.log(age);
 
-// Console Methods
-
-// Callstack
-
-// Grabbing Elements
-
-// Breakpoints
-
-// Scope
-
-// Network Requests
-
-// Break On Attribute
-
-// Some Setup Code
-
-function doctorize(name) {
-  return `Dr. ${name}`;
+if (1 === 1) {
+  const cool = true;
 }
 
-function greet(name) {
-  doesntExist();
-  return `Hello ${name}`;
-}
-
-function go() {
-  const name = doctorize(greet('Wes'));
-  console.log(name);
-}
-
-const button = document.querySelector('.bigger');
-button.addEventListener('click', function(e) {
-  const newFontSize =
-    parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
-  e.currentTarget.style.fontSize = `${newFontSize}px`;
-});
-
-// A Dad joke fetch
-async function fetchDadJoke() {
-  const res = await fetch('https://icanhazdadjoke.com/', {
-    headers: {
-      Accept: 'text/plain',
-    },
-  });
-  const joke = await res.text();
-  console.log(joke);
-  return joke;
-}
+console.log(cool);
