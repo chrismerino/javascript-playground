5654e99393c7cea59cffeca84b0a9c89e994c781
Chris Merino <cris.merino@live.com>
2020-07-27 15:48:26 -0600

    dom

--- the-dom.js
+++ the-dom.js
@@ -1,5 +1,23 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const heading = document.querySelector('h2');
 
-console.log(p);
-console.log(imgs);
+// console.log(p);
+// console.log(imgs);
+
+// console.log(heading.textContent);
+// heading.textContent = 'Chris';
+// console.log(heading.textContent);
+
+// Classes
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.toggle('round');
+
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
