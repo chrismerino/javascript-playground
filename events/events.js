d6734f7ae9146a4eb08b939b147527d7ef5739af
Chris Merino <cris.merino@live.com>
2020-07-28 13:26:45 -0600

    added basic event bindings

--- events/events.js
+++ events/events.js
@@ -1,2 +1,12 @@
 const butts = document.querySelector('.butts');
-butts.addEventListener('click', listener);
+const cool = document.querySelector('.cool');
+
+function handleClick() {
+  console.log('It got clickeeed!');
+}
+
+// Bindings
+butts.addEventListener('click', handleClick);
+cool.addEventListener('click', handleClick);
+
+butts.removeEventListener('click', handleClick);
