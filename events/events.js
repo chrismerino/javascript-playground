8081a1bc543aabe85c2cb4caab97551e392ea5c8
Chris Merino <cris.merino@live.com>
2020-07-28 15:41:26 -0600

    adding simple event handling clicks

--- events/events.js
+++ events/events.js
@@ -1,6 +1,9 @@
 const butts = document.querySelector('.butts');
 const cool = document.querySelector('.cool');
 
+// Listen on multiple items
+const buyButtons = document.querySelectorAll('button.buy');
+
 // Using a regular function
 function handleClick() {
   console.log('It got clickeeed!');
@@ -15,3 +18,11 @@ cool.addEventListener('click', handleClick);
 
 // Unbinding the elements
 butts.removeEventListener('click', hooray);
+
+function buyItem() {
+  console.log('Buying items...');
+}
+
+buyButtons.forEach(function(buyButton) {
+  buyButton.addEventListener('click', buyItem);
+});
