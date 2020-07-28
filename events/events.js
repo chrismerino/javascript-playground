d6b5f9f0d52afe675245d647211b1c0fe0255ccb
Chris Merino <cris.merino@live.com>
2020-07-28 13:30:08 -0600

    adding bindings

--- events/events.js
+++ events/events.js
@@ -1,12 +1,17 @@
 const butts = document.querySelector('.butts');
 const cool = document.querySelector('.cool');
 
+// Using a regular function
 function handleClick() {
   console.log('It got clickeeed!');
 }
 
+// Now using an arrow function
+const hooray = () => console.log('HOORAY!');
+
 // Bindings
-butts.addEventListener('click', handleClick);
+butts.addEventListener('click', hooray);
 cool.addEventListener('click', handleClick);
 
-butts.removeEventListener('click', handleClick);
+// Unbinding the elements
+butts.removeEventListener('click', hooray);
