f408296631273dd7c7c4216077185b2dff2a76b5
Chris Merino <cris.merino@live.com>
2020-07-29 09:55:34 -0600

    learning about different types of events and how to get data from a form input

--- events/forms.js
+++ events/forms.js
@@ -16,7 +16,25 @@ wes.addEventListener('click', function(event) {
 // This is a good way to gather info from a form input
 const signUpForm = document.querySelector('[name="signup"]');
 signUpForm.addEventListener('submit', function(event) {
-  console.log(event.currentTarget.name);
-  console.log(event.currentTarget.email);
-  event.preventDefault();
+  // The other way is this
+  console.log(event.currentTarget.querySelector('[name="email"]'));
+
+  // This is one way
+  // console.log(event.currentTarget.name.value);
+  // console.log(event.currentTarget.email.value);
+  // console.log(event.currentTarget.agree.checked);
+
+  const name = event.currentTarget.name.value;
+  if (name.includes('Chris')) {
+    alert('Sorry bro');
+    event.preventDefault();
+  }
 });
+
+function logEvents(event) {
+  console.log(event.type);
+  // console.log(event.currentTarget.value); Google this later!
+  console.log(event.target.value);
+}
+
+signUpForm.addEventListener('keyup', logEvents);
