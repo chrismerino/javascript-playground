8c2152c916fa4a53b6a80c2fbbe15fc7e6b80c7a
Chris Merino <cris.merino@live.com>
2020-07-29 09:16:16 -0600

    GOOD WAY TO GET INFO FROM A FORM INPUT

--- events/forms.js
+++ events/forms.js
@@ -1,1 +1,22 @@
-console.log('it works 2');
+const wes = document.querySelector('.wes');
+
+wes.addEventListener('click', function(event) {
+  event.preventDefault();
+  const shouldChangePage = confirm(
+    'This website might be malicious!, do you wish to proceed?'
+  );
+
+  if (!shouldChangePage) {
+    event.preventDefault();
+  }
+
+  console.log(shouldChangePage);
+});
+
+// This is a good way to gather info from a form input
+const signUpForm = document.querySelector('[name="signup"]');
+signUpForm.addEventListener('submit', function(event) {
+  console.log(event.currentTarget.name);
+  console.log(event.currentTarget.email);
+  event.preventDefault();
+});
