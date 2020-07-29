f4cc694fbb2d995caa55402548ced01a8e5b17ca
Chris Merino <cris.merino@live.com>
2020-07-29 11:25:11 -0600

    getting initial files ready

---
+++ etch-a-sketch/sketch.js
@@ -0,0 +1,11 @@
+// Select elements on the page - canvas, shake button
+
+// Setup canvas for drawing
+
+// Write a draw function
+
+// Write a handler for keys
+
+// Clear / Shake function
+
+// Listen for arrow keys
