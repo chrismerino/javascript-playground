b7c7f163c226f2763b0d04a2731fcfd100446705
Chris Merino <cris.merino@live.com>
2020-08-22 11:36:48 -0600

    adding base files

---
+++ currency-converter/app.js
@@ -0,0 +1,53 @@
+const dateIndicator = document.querySelector('#date');
+
+function getDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  return `${mm}/${dd}/${yyyy}`;
+}
+
+dateIndicator.textContent = getDate();
+
+const currencies = {
+  USD: 'United States Dollar',
+  AUD: 'Australian Dollar',
+  BGN: 'Bulgarian Lev',
+  BRL: 'Brazilian Real',
+  CAD: 'Canadian Dollar',
+  CHF: 'Swiss Franc',
+  CNY: 'Chinese Yuan',
+  CZK: 'Czech Republic Koruna',
+  DKK: 'Danish Krone',
+  GBP: 'British Pound Sterling',
+  HKD: 'Hong Kong Dollar',
+  HRK: 'Croatian Kuna',
+  HUF: 'Hungarian Forint',
+  IDR: 'Indonesian Rupiah',
+  ILS: 'Israeli New Sheqel',
+  INR: 'Indian Rupee',
+  JPY: 'Japanese Yen',
+  KRW: 'South Korean Won',
+  MXN: 'Mexican Peso',
+  MYR: 'Malaysian Ringgit',
+  NOK: 'Norwegian Krone',
+  NZD: 'New Zealand Dollar',
+  PHP: 'Philippine Peso',
+  PLN: 'Polish Zloty',
+  RON: 'Romanian Leu',
+  RUB: 'Russian Ruble',
+  SEK: 'Swedish Krona',
+  SGD: 'Singapore Dollar',
+  THB: 'Thai Baht',
+  TRY: 'Turkish Lira',
+  ZAR: 'South African Rand',
+  EUR: 'Euro',
+};
+
+function generateOptions(options) {
+  console.log(options);
+}
+
+const optionsHTML = generateOptions(currencies);
+console.log(optionsHTML);
